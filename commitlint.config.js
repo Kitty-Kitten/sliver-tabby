77cdb393743231cef0fde03591453d96963f52b7
HenryYang <sephenry@hotmail.com>
2020-10-11 19:56:53 +0800

    feat: add tools :building_construction:

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0, 'never'],
+    'body-max-length': [0, 'never'],
+    'subject-full-stop': [2, 'always', ':'],
+    'footer-max-length': [0, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'revert',
+        'build',
+        'chore',
+        'perf',
+        'dev',
+        'git',
+        'conf',
+      ],
+    ],
+  },
+};
