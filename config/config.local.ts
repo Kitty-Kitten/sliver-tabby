ea2b0fe80ee4eb627091d826d32a70dd45880313
SIYUAN YANG <sephenry@hotmail.com>
2020-10-15 22:22:07 +0800

    [feature]: add egg-mysql (#2)
    * feat: add egg-mysql :building_construction:
    
    * feat: add egg-mysql :building_construction:

--- config/config.local.ts
+++ config/config.local.ts
@@ -1,6 +1,11 @@
 import { EggAppConfig, PowerPartial } from 'egg';
+import { localConf } from '../db';
 
 export default () => {
-  const config: PowerPartial<EggAppConfig> = {};
+  const config: PowerPartial<EggAppConfig> = {
+    mysql: {
+      client: localConf,
+    },
+  };
   return config;
 };
